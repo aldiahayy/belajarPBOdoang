46daf4934b56422caaacd4ed8fd6577200dfd79c
aldi <aldisalam0756@gmail.com>
2026-01-10 06:45:26 +0800

    soal 5
    5. Buatlah program untuk menampilkan sejumlah n baris String “INFORMATIKA” dengan nilai variabel n diinput melalui console window.

---
+++ Praktikum 1/soal5.java
@@ -0,0 +1,12 @@
+package praktikum1;
+import java.util.Scanner;
+public class soal5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan jumlah baris (n): ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println("INFORMATIKA");
+        }
+    }
+}
