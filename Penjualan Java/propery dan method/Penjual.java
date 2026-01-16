0f95d2a3e5d30be95c52d6d4fbbdf97d0e7b675b
aldi <aldisalam0756@gmail.com>
2026-01-16 10:33:04 +0800

    class Penjual

---
+++ Penjualan Java/propery dan method/Penjual.java
@@ -0,0 +1,14 @@
+package com.myclass;
+public class Penjual {
+
+    // Propery
+    public String nama;
+    public int umur;
+    public String jenisKelamin;
+    public int lamaKerjaPerTahun;
+
+    // Method
+    public void cetakInfoPenjual(){
+        System.out.println("Nama Penjual : " + nama + ", Umur Penjual : " + umur + "tahun, Jenis Kelamin : " + jenisKelamin + ", Lama Kerja PerTahun : " + lamaKerjaPerTahun +"tahun.\n");
+    }
+}
