9609fb1d0107e01178859d8aef7cebb3c2ddfc09
aldi <aldisalam0756@gmail.com>
2026-01-16 10:27:06 +0800

    class Barang

---
+++ Penjualan Java/class dan object/Barang.java
@@ -0,0 +1,9 @@
+package myclass;
+// class Barang
+public class Barang {
+  
+  // method atau behavior
+    public void salam(){
+        System.out.println("sudah berhasil instansiasi dari class Barang");
+    }
+}
