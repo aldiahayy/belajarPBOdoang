d36c74c8b116f6d00ce5a8a582727ea1c6d70d17
aldi <aldisalam0756@gmail.com>
2026-01-16 10:29:47 +0800

    class Main

---
+++ Penjualan Java/class dan object/Main.java
@@ -0,0 +1,10 @@
+package program;
+import myclass.Barang;
+public class Main {
+    public static void main(String[] args) {
+        Barang barang1 = new Barang();
+      
+        //call mehtod salam
+        barang1.salam();
+    }
+}
