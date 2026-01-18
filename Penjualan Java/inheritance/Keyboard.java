b68ad715bafd454294c5f09fbec2078e7ad48412
aldi <aldisalam0756@gmail.com>
2026-01-19 05:47:28 +0800

    class Keyboard

---
+++ Penjualan Java/inheritance/Keyboard.java
@@ -0,0 +1,38 @@
+package com.myclass;
+public class Keyboard extends Barang{
+
+    // property
+    public String backlight = "tidak ada backlight";
+    public int banyakKeys = 0;
+    public String tipeSwitch = "tidak ada switch";
+
+    // constructor
+    public Keyboard(String namaBarang, int hargaBarang, int stokBarang, String backlight, int banyakKeys, String tipeSwitch){
+        this.namaBarang = namaBarang;
+        this.hargaBarang = hargaBarang;
+        this.stokBarang = stokBarang;
+        this.backlight = backlight;
+        this.banyakKeys = banyakKeys;
+        this.tipeSwitch = tipeSwitch;
+    }
+
+    // setter dan getter
+    public void setBacklight(String backlight){
+        this.backlight = backlight;
+    }
+    public String getBacklight(){
+        return this.backlight = backlight;
+    }
+    public void setBanyakKeys(int banyakKeys){
+        this.banyakKeys = banyakKeys;
+    }
+    public int getBanyakKeys(){
+        return this.banyakKeys = banyakKeys;
+    }
+    public void setTipeSwitch(String tipeSwitch){
+        this.tipeSwitch = tipeSwitch;
+    }
+    public String getTipeSwitch(){
+        return this.tipeSwitch = tipeSwitch;
+    }
+}
