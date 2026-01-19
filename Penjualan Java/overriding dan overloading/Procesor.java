6f5778dc5048b2b9ee6fa73b6a2e0e422b6793db
aldi <aldisalam0756@gmail.com>
2026-01-20 05:34:22 +0800

    class Procesor

---
+++ Penjualan Java/overriding dan overloading/Procesor.java
@@ -0,0 +1,35 @@
+package com.myclass;
+public class Procesor extends Barang{
+
+    // property
+    public double frekuensiDasar = 0;
+    public double turboBoost = 0;
+
+    // constructor
+    public Procesor(String namaBarang, int hargaBarang, int stokBarang, double frekuensiDasar, double turboBoost){
+        super(namaBarang, hargaBarang, stokBarang);
+        this.frekuensiDasar = frekuensiDasar;
+        this.turboBoost = turboBoost;
+    }
+
+    // setter dan getter
+    public void setFrekuensiDasar(double frekuensiDasar){
+        this.frekuensiDasar = frekuensiDasar;
+    }
+    public double getFrekuensiDasar(){
+        return this.frekuensiDasar = frekuensiDasar;
+    }
+    public void setTurboBoost(double turboBoost){
+        this.turboBoost = turboBoost;
+    }
+    public double getTurboBoost(){
+        return this.turboBoost = turboBoost;
+    }
+
+    // Overriding
+    @Override
+    public void cetakInfoBarang(){
+        super.cetakInfoBarang();
+        System.out.println(", Frekuensi Dasar : " + this.frekuensiDasar +", Turbo Boost : " + this.turboBoost + "\n");
+    }
+}
